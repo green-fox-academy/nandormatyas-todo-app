367e2da068ea8df74872cc9436341ec6d49ca2e3
nandormatyas <nandor.matyas@gmail.com>
2017-12-14 17:03:07 +0100

    feat(TODO): user friendly todo app

--- todo.py
+++ todo.py
@@ -2,16 +2,17 @@ from sys import *
 import os 
 
 class FileController():
+
     def __init__(self):
-        if len(argv) == 2 and argv[1] == "help":
+        if len(argv) >= 2 and argv[1] == "help":
             self.control_help()
-        elif len(argv) == 2 and argv[1] == '-l':
+        elif len(argv) >= 2 and argv[1] == '-l':
             self.list_tasks()
-        elif len(argv) == 2 and argv[1] == '-a':
+        elif len(argv) >= 2 and argv[1] == '-a':
             self.add_new_task()
-        elif len(argv) == 2 and argv[1] == '-r':
+        elif len(argv) >= 2 and argv[1] == '-r':
             self.remove_task()
-        elif len(argv) == 2 and argv[1] == '-c':
+        elif len(argv) >= 2 and argv[1] == '-c':
             self.complete_task()
 
     def control_help(self):
@@ -33,26 +34,35 @@ class FileController():
         if os.stat("todos.txt").st_size == 0:
             print('No todos for today! :)')
         else:
-            list_file = list_file.read()
-            print(list_file)
+            index = 1
+            list_file = list_file.readlines()
+            for i in list_file:
+                print(str(index) + str(i))
+                index += 1
 
     def add_new_task(self):
         list_file = open('todos.txt', 'a+')
         new_task = input('What new task would you like to add?: ')
-        list_file.write(' [ ]')
-        list_file.write(new_task)
-        list_file.write('\n')
-        list_file.close()
+        if len(new_task) < 1:
+            print('Unable to add: no task provided')
+        else:
+            list_file.write(' [ ]')
+            list_file.write(new_task)
+            list_file.write('\n')
+            list_file.close()
 
     def remove_task(self):
         list_file = open('todos.txt', 'r')
         lines = list_file.readlines()
         remove = str(input('Which task do you want to delete?: '))
-        list_file.close()
-        list_file = open('todos.txt', 'w')
-        for line in lines:
-            if line != ' [ ]' + remove + '\n' and line != ' [x]' + remove + '\n':
-                list_file.write(line)
+        if len(remove) < 1:
+            print('Unable to remove: no index provided')
+        else:
+            list_file.close()
+            list_file = open('todos.txt', 'w')
+            for line in lines:
+                if line != ' [ ]' + remove + '\n' and line != ' [x]' + remove + '\n':
+                    list_file.write(line)
         list_file.close()
 
     def complete_task(self):
